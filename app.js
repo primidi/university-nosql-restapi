79200bcf53877931577bff90f2b46a8eab8af0bf
primidi <hariyonogharis@gmail.com>
2022-02-06 18:53:25 +0700

    configure port

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ const studentRoute = require('./routes/student-route');
 require('./utils/db');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Main API's page
 app.get('/', (req, res) => {
-    res.send('Welcome to University API!');
+  res.send('Welcome to University API!');
 });
 
 // Student route middleware
 app.use('/api/mahasiswa', studentRoute);
 
 app.listen(port, () => {
-    console.log(`This test is listening at http://localhost:${port}`);
+  console.log(`This test is listening at http://localhost:${port}`);
 });
